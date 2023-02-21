db952a25b9a82400a184313c7f798f5682cce865
ShTanyaa <124123694+ShTanyaa@users.noreply.github.com>
2023-02-21 15:50:42 +0500

    Add files via upload

---
+++ Практика 19-ПТПМ/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace Практика_19_ПТПМ
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.checkBox3);
+            this.panel1.Controls.Add(this.checkBox2);
+            this.panel1.Controls.Add(this.checkBox1);
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Controls.Add(this.radioButton3);
+            this.panel1.Controls.Add(this.radioButton2);
+            this.panel1.Controls.Add(this.radioButton1);
+            this.panel1.Location = new System.Drawing.Point(29, 53);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(759, 229);
+            this.panel1.TabIndex = 0;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.checkBox3.Location = new System.Drawing.Point(24, 135);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(123, 28);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.checkBox2.Location = new System.Drawing.Point(24, 69);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(123, 28);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.checkBox1.Location = new System.Drawing.Point(24, 8);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(123, 28);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(445, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(311, 187);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(24, 167);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(136, 26);
+            this.radioButton3.TabIndex = 2;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "radioButton3";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(24, 103);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(136, 26);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "radioButton2";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            this.radioButton2.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Times New Roman", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(24, 42);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(136, 26);
+            this.radioButton1.TabIndex = 0;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "radioButton1";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            this.radioButton1.CheckedChanged += new System.EventHandler(this.radioButton1_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(26, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(57, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Вопросы";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(367, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Ответить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1213, 600);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Практика 19-ПТПМ/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Button;
+
+namespace Практика_19_ПТПМ
+{
+    public partial class Form1 : Form
+    {
+        string[,] question = new string[10, 6];
+        int curr_numb = 0, answer = 0;
+        Random rnd = new Random();
+        
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void radioButton4_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string[] a = question[curr_numb, 4].Split('/');
+            Double d = a.Length;
+            double h = 1 / d;
+            double y = 0;
+            if (a.Length > 1)
+            {
+
+
+                for (int j = 0; j < a.Length; j++)
+                {
+
+                    if (checkBox1.Checked)
+                    {
+                        if (checkBox1.Text == a[j])
+                        {
+                            answer++;
+                            y += h;
+                        }
+
+                    }
+                    if (checkBox2.Checked)
+                    {
+                        if (checkBox2.Text == a[j])
+                        {
+                            answer++;
+                            y += h;
+                        }
+
+                    }
+                    if (checkBox3.Checked)
+                    {
+                        if (checkBox3.Text == a[j])
+                        {
+                            answer++;
+                            y += h;
+                        }
+
+                    }
+                }
+            }
+            else
+            {
+
+
+                if (radioButton1.Checked)
+                {
+                    if (radioButton1.Text == question[curr_numb, 4])
+                    {
+                        answer++;
+                    }
+                }
+                if (radioButton2.Checked)
+                {
+                    if (radioButton2.Text == question[curr_numb, 4])
+                    {
+                        answer++;
+                    }
+                }
+                if (radioButton3.Checked)
+                {
+                    if (radioButton3.Text == question[curr_numb, 4])
+                    {
+                        answer++;
+                    }
+                }
+            }
+
+            if (curr_numb < 9)
+            {
+                curr_numb++;
+                this.Text = "вопрос " + (curr_numb + 1);
+                label1.Text = question[curr_numb, 0];
+                if (question[curr_numb, 5] != "")
+                {
+                    pictureBox1.Visible = true;
+                    pictureBox1.Image = Image.FromFile(question[curr_numb, 5]);
+                }
+                else pictureBox1.Visible = false;
+                a = question[curr_numb, 4].Split('/');
+
+                if (a.Length > 1)
+                {
+                    
+                    radioButton1.Visible = false;
+                    radioButton2.Visible = false;
+                    radioButton3.Visible = false;
+                    checkBox1.Visible = true;
+                    checkBox2.Visible = true;
+                    checkBox3.Visible = true;
+                    checkBox1.Text = question[curr_numb, 1];
+                    checkBox2.Text = question[curr_numb, 2];
+                    checkBox3.Text = question[curr_numb, 3];
+                    
+                }
+                else
+                {
+
+                    radioButton1.Visible = true;
+                    radioButton2.Visible = true;
+                    radioButton3.Visible = true;
+                    checkBox1.Visible = false;
+                    checkBox2.Visible = false;
+                    checkBox3.Visible = false;
+                    radioButton1.Text = question[curr_numb, 1];
+                    radioButton2.Text = question[curr_numb, 2];
+                    radioButton3.Text = question[curr_numb, 3];
+
+                }
+            }
+            else
+            {
+                button1.Enabled = false;
+                if (curr_numb + 1 == answer)
+                {
+                    MessageBox.Show("вы ответили  на все " + answer + " вопросов", "результаты");
+                }
+                else
+                {
+                    MessageBox.Show($"вы ответили верно на  {answer} вопросов", "результаты");
+                }
+            }
+        }
+
+    
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = File.OpenText("quest.txt");
+            int i = 0; int j;
+            while (!sr.EndOfStream)
+            {
+                for (j = 0; j < 6; j++)
+                {
+                    question[i, j] = sr.ReadLine();
+                }
+                i++;
+            }
+            sr.Close();
+            this.Text = "вопрос " + (curr_numb + 1);
+            label1.Text = question[0, 0];
+            radioButton1.Text = question[0, 1];
+            radioButton2.Text = question[0, 2];
+            radioButton3.Text = question[0, 3];
+            if (question[0, 5] != "")
+            {
+                pictureBox1.Image = Image.FromFile(question[0, 5]);
+            }
+            else pictureBox1.Visible = false;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
